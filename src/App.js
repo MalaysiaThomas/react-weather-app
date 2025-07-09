3dd36e937e90762163b4c8343ba794383431f709
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-07-09 16:25:36 -0400

    Add footer with credits and links to App
    Introduced a footer section in the App component that provides author credit, links to the GitHub repository, and hosting information.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-     
+
+      <footer>
+        <p>
+          Created by
+          <a href="https://github.com/MalaysiaThomas"> Malaysia Thomas</a> and is open-sourced on <a href="https://github.com/MalaysiaThomas/react-weather-app">GitHub</a>. Hosted on <a href="https://react-weather-app-aeris.netlify.app/">Netlify</a>.
+        </p>
+      </footer>
     </div>
   );
 }
